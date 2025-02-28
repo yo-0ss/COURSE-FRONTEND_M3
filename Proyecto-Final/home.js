32720020a52677ecb3d8f8235a1c4ce99b7e6e00
yo-0ss <yoss.rios27@gmail.com>
2025-02-27 22:43:08 -0600

    Agrego trabajo realizado en la clase del 20 de Febrero y estilos extra a la página

--- Proyecto-Final/home.js
+++ Proyecto-Final/home.js
@@ -1,8 +1,18 @@
 
-function post() {
+document.addEventListener("DOMContentLoaded", function() {
+    const user = JSON.parse(localStorage.getItem("loggedUser"));
+
+    if(!user){
+        window.location.href = "index.html";
+        return
+    }
+
+    mostrarComentarios();
+})
+
+function postComment() {
     const commentText = document.getElementById('comment').value;
-    const commentSection = document.getElementById('comment-section');
-    const userName = localStorage.getItem("loggedUser")
+    const user = JSON.parse(localStorage.getItem("loggedUser"));
     const fecha = new Date().toLocaleString();
 
     if (commentText.trim() === "") { 
@@ -10,10 +20,50 @@ function post() {
         return;
     }
 
-    const comentarioSection = document.createElement("section");
-    comentarioSection.classList.add("comentario");
-    comentarioSection.innerHTML = `<strong>${userName}</strong> - ${fecha} <br> ${commentText}`;
+    const newComment = {
+        username: user.username,
+        name: user.name,
+        image: user.image,
+        commentText: commentText,
+        timestamp: fecha
+    }
 
-    commentSection.appendChild(comentarioSection)
-    document.getElementById('comment').value = ""; 
+    let comments = JSON.parse(localStorage.getItem('comments')) || [];
+    comments.push(newComment);
+
+    localStorage.setItem('comments', JSON.stringify(comments));
+
+    document.getElementById('comment').value = ""
+
+    mostrarComentarios();
 }
+
+function mostrarComentarios() {
+    const commentSection = document.getElementById('comment-section');
+    let comments = JSON.parse(localStorage.getItem('comments')) || [];
+
+    commentSection.innerHTML = '';
+
+    comments.forEach(comment => {
+        const comentarioSection = document.createElement("section");
+        comentarioSection.classList.add("comentario");
+        comentarioSection.innerHTML = `<section class="datos-user">
+        <img src="${comment.image}" class="avatar" alt="avatar">
+        <strong>${comment.name}</strong> </section>
+        <p class="fecha">${comment.timestamp}</p>
+        <p class="texto">${comment.commentText}</p>`;
+        
+        let botonEliminar = document.createElement("button");
+        botonEliminar.innerText= `Eliminar`
+
+        botonEliminar.onclick = function () {
+            comentarioSection.remove();
+            comments = comments.filter(c => c.timestamp !== comment.timestamp);
+            localStorage.setItem('comments', JSON.stringify(comments));
+            mostrarComentarios();
+        };
+        comentarioSection.appendChild(botonEliminar);
+        commentSection.appendChild(comentarioSection)
+    });
+}
+
--- Proyecto-Final/script.js
+++ Proyecto-Final/script.js
@@ -1,17 +1,22 @@
-//user
-const yossUser = "yoss"
-const yossPassword = "1234"
+const users = [
+    {name: "Yosselin Ríos", username: "yo-oss", password: "1234", image: "https://th.bing.com/th/id/OIP.7nbqumECd0QnwmceT3AHrAHaHj?w=736&h=750&rs=1&pid=ImgDetMain"},
+    {name: "Ivan García", username: "ivanfou", password: "abcd", image:"https://images.pexels.com/photos/19651067/pexels-photo-19651067/free-photo-of-plato-postre-dulce-pastel.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+    {name: "Mariana López", username: "marilopez", password: "pass123", image: "https://images.pexels.com/photos/3763188/pexels-photo-3763188.jpeg?auto=compress&cs=tinysrgb&w=600"},  
+    {name: "Carlos Méndez", username: "cmendez99", password: "qwerty", image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"}
+]
 
 function LogIn() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const usernamePage = document.getElementById('username').value;
+    const passwordPage = document.getElementById('password').value;
     const error = document.getElementById('error');
 
-    if (username === yossUser && password === yossPassword) {
-        localStorage.setItem("loggedUser", username)
+    const userFind = users.find(user => user.username === usernamePage && user.password === passwordPage)
+
+    if (userFind) {
+        localStorage.setItem("loggedUser", JSON.stringify(userFind))
         window.location.href = "home.html"
     } else {
-        error.textContent = "Usuario o contraseña incorrecto"        
+        error.textContent = "*Usuario o contraseña incorrecto"        
     }
 
 }
